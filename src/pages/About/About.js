8f20e6605c5024c245ae24390891e4bc60c1e52e
danny p <danny@dannys-MacBook-Pro.local>
2022-04-21 16:43:15 -0400

    bio years

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -18,10 +18,27 @@ const About = () => {
          </div>
        </div>
        <div className="about-cont">
-         <p className="about-title">About Me</p>
-         <p>I'm a full-stack developer based in Miami Lakes, Florida with a passion for both front-end and back-end engineering.
+         <h5 className="about-title">About Me</h5>
+         <p className="bio-body">I'm a full-stack developer based in Miami Lakes, Florida with a passion for both front-end and back-end engineering.
             When I'm not online you can most likely find me at the gym or playing video games. If you want to see more, check out my portfolio. </p>
          <Link className="work-link" to="/work">My portfolio ›</Link>
+         <div className="bio-body-cont">
+            <h5 className="bio-body-title">Bio</h5>
+            <div className="facts-cont">
+            <div className="fact-cont">
+            <p class="year">1996</p>
+            <p className="facts"> Born, raised in Miami, Florida.</p>
+            </div>
+            <div className="fact-cont">
+            <p class="year">2020</p>
+            <p className="facts"> Graduates Florida International University with a Bachelors in International Business.</p>
+            </div>
+            <div className="fact-cont">
+            <p class="year">2022</p>
+            <p className="facts"> Graduates University of Miami Full Stack Development Bootcamp after 6 months..</p>
+            </div>
+            </div>
+         </div>
        </div>
   </main>
   
