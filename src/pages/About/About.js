5fe64c004251cf94e043b7e12e46f325abee118f
danny p <danny@dannys-MacBook-Pro.local>
2022-04-21 14:51:18 -0400

    working on bio

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,6 @@
 
 import './about.css'
-
+import headshot from '../../images/headshot.png'
 
 const About = () => {
 
@@ -8,6 +8,15 @@ const About = () => {
     return (
       <main>
        <div className="intro">Hello, I'm a full-stack developer based in Miami!</div>
+       <div className="bio-title">
+         <div className="name-title">
+           <p className="name">Hector Perez</p>
+           <p className="title">Developer (Fullstack)</p>
+         </div>
+         <div className="picture-cont">
+           <img alt="headshot-of-me" className="avi-img" src={headshot} />
+         </div>
+       </div>
   </main>
   
     );
