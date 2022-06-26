720839b6d877ca7741ba8620d4e393f50772c353
Hector <hector.pere102@gmail.com>
2022-06-26 16:34:55 -0400

    changed words in about me sextion

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
  
     return (
       <main>
-       <div className="intro">Hello, I'm a full-stack developer based in Miami!</div>
+       <div className="intro">Hello, I'm a software developer based in Miami!</div>
        <div className="bio-title">
          <div className="name-title">
            <p className="name">Hector Perez</p>
@@ -19,7 +19,7 @@ const About = () => {
        </div>
        <div className="about-cont">
          <h5 className="about-title">About Me</h5>
-         <p className="bio-body">I'm a full-stack developer based in Miami Lakes, Florida with a passion for both front-end and back-end engineering.
+         <p className="bio-body">I'm a software developer based in Miami Lakes, Florida with a passion for both front-end and back-end engineering.
             When I'm not online you can most likely find me at the gym or playing video games. If you want to see more, check out my portfolio. </p>
          <Link className="work-link" to="/work">My portfolio ›</Link>
          <div className="bio-body-cont">
